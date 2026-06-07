73271f2dd9a794f1c57050b1344ac7430f7249d4
Yin Boran <33853279+YinBoran@users.noreply.github.com>
2026-06-07 11:10:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js — replace with NEW URL:
-window.API = "window.API = "https://yinboran-github-io.boranyin24.workers.dev";";
+window.API = "window.API = "https://yinboran-github-io.boranyin24.workers.dev";
