531451ca42cd079b055f590c73369042a5bb89d5
Yin Boran <33853279+YinBoran@users.noreply.github.com>
2026-06-07 11:12:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ✅ Fix — add both:
-const API = "https://yinboran-github-io.boranyin24.workers.dev";
+const API = "https://script.google.com/macros/s/AKfycbzed-sl6kTHLqf5JKBp7vxl2v89hF_--hMAf33E3gceROoUsZp092rmQILFa4YmEITj/exec";
 window.API = API;
